9deb213a7a3f2ee0181c2729049ad7653b7957c9
ahmadabdallahh <ahmadabhossen@gmail.com>
2025-04-03 11:24:43 +0200

    Add iterative and recursive implementations of the factorial function with Big O complexity comments; include console tests for both solutions.

--- 08 - Factorial of a Number/Factorial-of-a-Number.js
+++ 08 - Factorial of a Number/Factorial-of-a-Number.js
@@ -10,11 +10,26 @@
 */
 
 function Factorial(n) {
-  let intiValue = 1;
+  let intiValue = 1; // Big O: O(1)
   for (let i = 2; i <= n; i++) {
-    intiValue *= i;
+    // Big O: O(n)
+    intiValue *= i; // Big O: O(1)
   }
-  return intiValue;
+  return intiValue; // Big O: O(1)
 }
 
-console.log(Factorial(5));
+console.log(Factorial(5)); // Big O: O(n)
+
+/* Solution 2 */
+
+/*
+    1- Check if the number is less than or equal to 1
+    2- If it is, return 1
+    3- Otherwise, return the number multiplied by the factorial of the number minus 1
+*/
+
+function FactorialTwo(n) {
+  return n <= 1 ? 1 : n * FactorialTwo(n - 1); // Big O: O(n)
+}
+
+console.log(FactorialTwo(0));
